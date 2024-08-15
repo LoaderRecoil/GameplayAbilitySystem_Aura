2e4e03aabcd252d73797cbbaeebb0f1459f02433
Andrew Casey <a.casey111@hotmail.com>
2024-08-15 15:04:55 +0100

    Attribute Menu PT 2
    Setup Data Table for Primary/Secondary Attributes for use in Attribute Menu Widget
    Changed to Native Gameplay Tags for All Attributes

---
+++ Source/Aura/Private/UI/WigetController/AttributeWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright LoaderRecoil
+
+
+#include "UI/WigetController/AttributeWidgetController.h"
+
+void UAttributeWidgetController::BindCallbacksToDependencies()
+{
+
+}
+
+void UAttributeWidgetController::BroadcastInitialValues()
+{
+
+}
