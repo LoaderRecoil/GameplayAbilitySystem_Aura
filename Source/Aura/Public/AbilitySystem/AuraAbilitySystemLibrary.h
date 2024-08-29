77e4ba60bf9954d7439aa3868861a8ad42f27b1c
Andrew Casey <a.casey111@hotmail.com>
2024-08-29 15:02:02 +0100

    Characters Complete
    3 Base Classes (Elementalist, Warrior, Ranger)
    All classes have curve tables for stat changes per level increase
    classes can be used for both player and enemies

--- Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
+#include "Data/CharacterClassInfo.h"
 #include "AuraAbilitySystemLibrary.generated.h"
 
 class UAttributeMenuWidgetController;
@@ -23,4 +24,8 @@ public:
 	
 	UFUNCTION(BlueprintPure, Category = "AuraAbilitySystemLibrary|WidgetController")
 	static UAttributeMenuWidgetController* GetAttributeMenuWidgetController(const UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|CharacterClassDefaults")
+	static void InitializeDefaultAttributes(const UObject* WorldContextObject, 
+		ECharacterClass CharacterClass, float Level, UAbilitySystemComponent* ASC);
 };
