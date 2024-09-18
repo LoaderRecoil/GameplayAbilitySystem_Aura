9a526b06121110bf8034d69983952c3094d7a7fe
Andrew Casey <a.casey111@hotmail.com>
2024-09-18 17:24:38 +0100

    Enemy Spell Attacks Done
    New enemy "Shaman"
    Uses fire projectiles (Same as player)
    Added Death to Blackboard (Enemies could still fire projectiles during dissolve phase(Already Defeated))

--- Source/Aura/Private/Actor/AuraProjectile.cpp
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -62,10 +62,7 @@ void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent,
 	AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
 	bool bFromSweep, const FHitResult& SweepResult)
 {
-
-	// if (!DamageEffectSpecHandle.Data.IsValid() || 
-	// DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser() == OtherActor)
-	if (DamageEffectSpecHandle.Data.IsValid() &&
+	if (!DamageEffectSpecHandle.Data.IsValid() ||
 		DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser() == OtherActor)
 	{
 		return;
