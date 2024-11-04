3b49afd468fd8d4baaf04d2ab4c630f94cbb5f16
Andrew Casey <a.casey111@hotmail.com>
2024-11-04 17:40:03 +0000

    Spell Menu Completed
    Very long section.
    Ensuring everything is working as intended.

--- Source/Aura/Public/UI/WigetController/SpellMenuWidgetController.h
+++ Source/Aura/Public/UI/WigetController/SpellMenuWidgetController.h
@@ -10,6 +10,7 @@
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams(FSpellGlobeSelectedSignature, bool, bSpendPointsButtonEnabled, bool, bEquipButtonEnabled, FString, DescriptionString, FString, NextLevelDescriptionString);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FWaitForEquipSelectionSignature, const FGameplayTag&, AbilityType);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FSpellGlobeReassignedSignature, const FGameplayTag&, AbilityTag);
 
 struct FSelectedAbility
 {
@@ -42,6 +43,9 @@ public:
 	UPROPERTY(BlueprintAssignable)
 	FWaitForEquipSelectionSignature StopWaitingForEquipDelegate;
 
+	UPROPERTY(BlueprintAssignable)
+	FSpellGlobeReassignedSignature SpellGlobeReassignedDelegate;
+
 	UFUNCTION(BlueprintCallable)
 	void SpellGlobeSelected(const FGameplayTag& AbilityTag);
 
@@ -54,6 +58,12 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void EquipButtonPressed();
 
+	UFUNCTION(BlueprintCallable)
+	void SpellRowGlobePressed(const FGameplayTag& SlotTag, const FGameplayTag& AbilityType);
+
+	void OnAbilityEquipped(const FGameplayTag& AbilityTag, const FGameplayTag& Status, 
+		const FGameplayTag& Slot, const FGameplayTag& PreviousSlot);
+
 private:
 
 	static void ShouldEnableButtons(const FGameplayTag& AbilityStatus, int32 SpellPoints,
@@ -63,5 +73,8 @@ private:
 		FAuraGameplayTags::Get().Abilities_Status_Locked };
 
 	int32 CurrentSpellPoints = 0;
+
 	bool bWaitingForEquipSelection = false;
+
+	FGameplayTag SelectedSlot;
 };
